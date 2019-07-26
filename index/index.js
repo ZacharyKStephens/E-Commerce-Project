0d9c54e4b552ff8c64bdb0cec961a12f4ca6609d
Zachary Kaleb Stephens <zacharykalebstephens@AdminisatorsMBP.attlocal.net>
2019-07-26 00:53:37 -0400

    changes song for index

---
+++ index/index.js
@@ -0,0 +1,13 @@
+
+ $('.slider').bxSlider({
+    autoControls: true,
+    auto: true,
+    pager: true,
+    mode: 'fade',
+    captions: true,
+    speed: 1000,
+    
+});
+window.onload = function() {
+    var context = new AudioContext();
+  }
